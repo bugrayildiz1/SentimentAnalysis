907f7baa4c6cafc306d1a30577f10c2470a1104f
bugrayildiz <bugrayildiz22@gmail.com>
2023-01-19 23:25:12 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import pprint
+
+
+api_key = '6be098ce1f644d6abe8d914f420af088'
+
+TRANSCRIPT_ENDPOINT = 'https://api.assemblyai.com/v2/transcript/rutxp2wyod-e574-4d82-921a-dc9994f3254b'
+
+response = requests.get(
+      TRANSCRIPT_ENDPOINT,
+      headers={'authorization': api_key},
+      json={
+    	'audio_url': 'https://www.americanrhetoric.com/mp3clipsXE/politicalspeeches/joebideninauguraladdressARXE.mp3',
+        'sentiment_analysis': True
+      },
+)
+  
+response_json = response.json()
+print(pprint.pprint(response_json))
